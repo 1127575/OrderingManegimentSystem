e1821287bb921267f88e5b27306dc1ee26c29d0d
TomoakiSuzuki <sbc2.t.s@gmail.com>
2020-06-09 13:41:39 +0900

    コントローラーを追加

---
+++ OrderingManegimentSystem/Controllers/ItemMaintenanceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OrderingManegimentSystem.Controllers
+{
+    public class ItemMaintenanceController : Controller
+    {
+        // GET: ItemMaintenance
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
