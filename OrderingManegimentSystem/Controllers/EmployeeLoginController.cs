271df3aa396fc1b1c88d30b18e62fcdc4e274255
克幸 照屋 <teruya00000@gmail.com>
2020-06-09 13:57:53 +0900

    コントローラーを追加

---
+++ OrderingManegimentSystem/Controllers/EmployeeLoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OrderingManegimentSystem.Controllers
+{
+    public class EmployeeLoginController : Controller
+    {
+        // GET: EmployeeLogin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
