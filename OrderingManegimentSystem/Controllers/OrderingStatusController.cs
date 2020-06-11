e01a3325d67c0f2cd99848036de8483b24313def
ayaka horii <ayaka.h.626@gmail.com>
2020-06-11 15:52:50 +0900

    UC07、UC09のバックアップ。

--- OrderingManegimentSystem/Controllers/OrderingStatusController.cs
+++ OrderingManegimentSystem/Controllers/OrderingStatusController.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OrderingManegimentSystem.Models;
+
 
 namespace OrderingManegimentSystem.Controllers
 {
     public class OrderingStatusController : Controller
     {
+        Database1Entities db = new Database1Entities();
         // GET: OrderingStatus
-        public ActionResult Index()
+        public ActionResult Orderstatus(int CustomerId = 1)
+        {
+            using (var db = new Database1Entities())
+            {
+                var ol = from e in db.OrderDetails where e.CustomerId == CustomerId select e;
+                //return Content(string.Join("<br>", ol));
+                return View(ol);
+            }
+        }
+        public ActionResult Ordercancel(int OrderNo)
         {
             return View();
         }
