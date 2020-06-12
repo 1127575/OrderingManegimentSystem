7553b3c22fd149910451a1658bdde962d5c6bf2f
ayaka horii <ayaka.h.626@gmail.com>
2020-06-12 15:21:36 +0900

    UC09の途中。検索試行錯誤中。

--- OrderingManegimentSystem/Controllers/OrderingSearchController.cs
+++ OrderingManegimentSystem/Controllers/OrderingSearchController.cs
@@ -18,25 +18,41 @@ namespace OrderingManegimentSystem.Controllers
         {
             using (var db = new Database1Entities())
             {
+                var dl = from a in db.OrderDetails
+                         select a;
+                //［顧客ID］欄が空でない場合、その値で部分一致検索
+                if (!string.IsNullOrEmpty(customerId))
+                {
+                    dl = db.OrderDetails.Where(a => a.Contains(customerId));
+                }
 
-                var dl = from o in db.OrderDetails//外部結合P236
-                         join p in db.Products on o.ItemNo equals p.ItemNo
-                         into z
-                         from subd in z.DefaultIfEmpty()
-                         select new
-                         {
-                             orderdetail = o.OrderNo + "-" + o.DetailNo,
-                             itemNo = o.ItemNo,
-                             Product = new
-                             {
-                                 itemNo = o.ItemNo,
-                                 itemName = (subd == null ?
-                                                        String.Empty : subd.ItemName)
-                             },
-                             quantity = o.Quantity,
-                             deliveryDate = o.DeliveryDate,
-                             status = o.Status,
-                         };
+                //［公開済］チェックが付いている場合、公開済みの記事だけを絞り込み
+                if (released.HasValue && released.Value)
+                {
+                    articles = articles.Where(a => a.Released);
+                }
+                /*var dl = from e in db.OrderDetails
+                                          where e.CustomerId == customerId
+                                          & e.OrderNo == orderNo
+                                          //& deliveryfrom >= e.DeliveryDate >= deliveryTo
+                                          //& orderFrom >= e.OrderDate >= orderTo
+                                          & e.Status == status
+                                          select e;
+
+                    ViewBag.result = from o in db.OrderDetails//内部結合P231
+                                            join p in db.Products on o.ItemNo equals p.ItemNo
+                                            select new
+                                            {
+                                                orderdetail = o.OrderNo + "-" + o.DetailNo,
+                                                itemNo = o.ItemNo,
+                                                Product = new
+                                                {
+                                                    itemName = p.ItemName
+                                                },
+                                                quantity = o.Quantity,
+                                                deliveryDate = o.DeliveryDate,
+                                                status = o.Status,
+                                            };*/
                 return View(dl);
             }
         }
