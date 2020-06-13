b5072b6c2a184d641225ca1b2b385f33014691d1
ayaka horii <ayaka.h.626@gmail.com>
2020-06-13 18:34:42 +0900

    受注情報検索機能。 ・日付の検索 ・入力されてるものだけで検索 ・検索結果をViewで表示 が残ってる。

--- OrderingManegimentSystem/Controllers/OrderingSearchController.cs
+++ OrderingManegimentSystem/Controllers/OrderingSearchController.cs
@@ -15,58 +15,46 @@ namespace OrderingManegimentSystem.Controllers
         {
             return View();
         }
-        public ActionResult Searchresult(int? customerId, int? orderNo, DateTime? deliveryFrom, DateTime? deliveryTo, DateTime? orderFrom, DateTime? orderTo, string status)
-        //public ActionResult Searchresult(string customerId, string orderNo, string deliveryFrom, string deliveryTo, string orderFrom, string orderTo, string status)
+        //public ActionResult Searchresult(int? customerId, int? orderNo, DateTime? deliveryFrom, DateTime? deliveryTo, DateTime? orderFrom, DateTime? orderTo, string status)
+        public ActionResult Searchresult(string customerId, string orderNo, string deliveryFrom, DateTime? deliveryTo, string orderFrom, string orderTo, string status)
         {
             using (var db = new Database1Entities())
             {
-                //if (customerId != 0)//検索条件の表示
-                if (customerId != null)//検索条件の表示
-                {
-                    ViewBag.name1 = "顧客ID";
-                    ViewBag.customerId = customerId;
-                }
-                //if(orderNo != 0)
-                if (orderNo != null)
-                {
-                    ViewBag.name2 = "注文番号";
-                    ViewBag.orderNo = orderNo;
-                }
-                if ((deliveryFrom != null) || (deliveryTo != null))
-                {
-                    ViewBag.name3 = "希望納期";
-                    ViewBag.deliveryPeriod = deliveryFrom + "～" + deliveryTo;
-                }
-                if ((orderFrom != null) || (orderTo != null))
-                {
-                    ViewBag.name4 = "受注日時";
-                    ViewBag.orderPeriod = orderFrom + "～" + orderTo;
-                }
-
+                ViewBag.customerId = customerId;
+                ViewBag.orderNo = orderNo;
+                ViewBag.deliveryPeriod = deliveryFrom + "～" + deliveryTo;
+                ViewBag.orderPeriod = orderFrom + "～" + orderTo;
                 ViewBag.status = status;
 
                 //検索＆内部結合
                 //OrderDetail内でCustomerIdが入力と一致するものを探し、ItemNoをもとにProductと結合させる。
-                var result = from o in db.OrderDetails 
-                             where o.CustomerId == customerId//部分一致に変える。期間系入れる。
-                             & o.OrderNo == orderNo
-                             & o.Status == status
-                             join p in db.Products on o.ItemNo equals p.ItemNo
-                                 select new
-                                 {
-                                     orderdetail = o.OrderNo + "-" + o.DetailNo,
-                                     itemNo = o.ItemNo,
-                                     Product = new
-                                     {
-                                         itemName = p.ItemName
-                                     },
-                                     quantity = o.Quantity,
-                                     deliveryDate = o.DeliveryDate,
-                                     status = o.Status,
-                                 };
+                var result = db.OrderDetails.Where(
+                    o => o.CustomerId.ToString().Contains(customerId)
+                    & o.OrderNo.ToString().Contains(orderNo)//期間系入れる。
+                   //& o.DeliveryDate BETWEEN(deliveryFrom) AND(deliveryTo)
+                    & o.Status == status
+                    ).Join(
+                    db.Products,
+                    o => o.Product.ItemNo,
+                    p => p.ItemNo,
+                    (o, p) =>
+                    new
+                    {
+                        orderdetail = o.OrderNo + "-" + o.DetailNo,
+                        itemNo = o.ItemNo,
+                        Product = new
+                        {
+                            itemName = p.ItemName
+                        },
+                        quantity = o.Quantity,
+                        deliveryDate = o.DeliveryDate,
+                        status = o.Status,
+                    }
+                    );
+
                 ViewBag.result = result;
-                return View();
-                //return Content(string.Join("<br>", result));
+                //return View();
+                return Content(string.Join("<br>", result));
             }
         }
     }
