e8e192ba0bd9d4967111115660dce6f05b732b83
克幸 照屋 <teruya00000@gmail.com>
2020-06-18 10:10:59 +0900

    UC02,UC08（仮）

--- OrderingManegimentSystem/Controllers/CustomerLoginController.cs
+++ OrderingManegimentSystem/Controllers/CustomerLoginController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OrderingManegimentSystem.Models;
 
 namespace OrderingManegimentSystem.Controllers
 {
@@ -13,9 +14,32 @@ namespace OrderingManegimentSystem.Controllers
         {
             return View();
         }
+        [HttpGet]
         public ActionResult CustomerMainMenu()
         {
-            return View();
+            return View("CustomerLoginIndex");
+        }
+        [HttpPost]
+        public ActionResult CustomerMainMenu(Customer customer)
+        {
+            using (var db = new Database1Entities())
+            {
+                var ul = db.Customers.Find(customer.CustomerId);
+                int t = customer.CustomerId;
+                string p = customer.Password;
+                if (t == ul.CustomerId && customer.Password == ul.Password)
+                {
+                    //Session["顧客名"] = ul.CustomerName;
+                    return View();
+                }
+                else
+                {
+                    ViewBag.IsAuth = false;
+                    return View("CustomerLoginIndex");
+                }
+            }
+
+
         }
     }
 }
--- OrderingManegimentSystem/Controllers/EmployeeLoginController.cs
+++ OrderingManegimentSystem/Controllers/EmployeeLoginController.cs
@@ -3,15 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OrderingManegimentSystem.Models;
 
 namespace OrderingManegimentSystem.Controllers
 {
     public class EmployeeLoginController : Controller
     {
         // GET: EmployeeLogin
-        public ActionResult Index()
+        public ActionResult Login()
         {
             return View();
+        }
+        public ActionResult EmployeeMain(Employee employee)
+        {
+            using (var db = new Database1Entities())
+            {
+                var ul = db.Customers.Find(employee.EmpNo);
+                int t = employee.EmpNo;
+                string p = employee.Password;
+                if (t == ul.CustomerId && p == ul.Password)
+                {
+                    //Session["顧客名"] = ul.CustomerName;
+                    return View();
+                }
+                else
+                {
+                    ViewBag.IsAuth = false;
+                    return View("CustomerLoginIndex");
+                }
+            }
         }
     }
 }
